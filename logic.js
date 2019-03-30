78123cf27804c105788b06a0ce5d82ce67d86fc4
dxvo <dxvo@uh.edu>
2019-03-30 01:09:10 -0500

    final

--- logic.js
+++ logic.js
@@ -20,13 +20,6 @@ function createFeatures(All_data) {
       "<p>Magnitude: " + feature.properties.mag + "</p>");
   }  
 
-function getGreenToRed(percent){
-            r = percent<50 ? 255 : Math.floor(255-(percent*2-100)*255/100);
-            g = percent>50 ? 255 : Math.floor((percent*2)*255/100);
-            return 'rgb('+r+','+g+',0)';
-        }
-
-
 
   // Create a GeoJSON layer containing the features of each quake location
   var geojsonLayer = L.geoJSON(All_data, {
