961b4d80c21e89ee8edcf1e553270be1e3eb988c
dxvo <dxvo@uh.edu>
2019-03-30 01:02:10 -0500

    final

--- logic.js
+++ logic.js
@@ -1,54 +1,64 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+//all week
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 var query2 = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson"
 
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
+//get data
+d3.json(url, function(response) {
+  createFeatures(response.features);
 });
 
-function createFeatures(earthquakeData) {
+
+function createFeatures(All_data) {
 
 
-  // Give each feature a popup describing the place and time of the earthquake
+  // use on each to bind popup info for each  location
   function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
-      "</h3><hr><p>Magnitude: " + feature.properties.mag + "</p>");
-  }
+    layer.bindPopup("<p>" + feature.properties.place +
+      "</p><p>" + new Date(feature.properties.time) + "</p>" +
+      "<p>Magnitude: " + feature.properties.mag + "</p>");
+  }  
 
-  
+function getGreenToRed(percent){
+            r = percent<50 ? 255 : Math.floor(255-(percent*2-100)*255/100);
+            g = percent>50 ? 255 : Math.floor((percent*2)*255/100);
+            return 'rgb('+r+','+g+',0)';
+        }
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
+
+
+  // Create a GeoJSON layer containing the features of each quake location
+  var geojsonLayer = L.geoJSON(All_data, {
+
     onEachFeature: onEachFeature,
+
+    //red (255,0,0)
+    //hue (0 - 100) - 0. is red
     pointToLayer: function (feature, latlng) {
       var color;
-      var r = 255;
-      var g = Math.floor(255-80*feature.properties.mag);
-      var b = Math.floor(255-80*feature.properties.mag);
-      color= "rgb("+r+" ,"+g+","+ b+")"
+      var percent = (feature.properties.mag/6)*100;
+      var g = percent<20 ? 255 : Math.floor(255-(percent*2-100)*255/100);
+      var r = percent> 85 ? 255 : Math.floor((percent*2)*255/100);
+      color = 'rgb('+r+','+g+',0)';
       
-      var geojsonMarkerOptions = {
-        radius: 4*feature.properties.mag,
+      var MarkerOptions = {
+        radius: 4.5*feature.properties.mag,
         fillColor: color,
         color: "black",
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8
+        fillOpacity: 1
       };
-      return L.circleMarker(latlng, geojsonMarkerOptions);
+      return L.circleMarker(latlng, MarkerOptions);
     }
   });
 
-
   // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
+  createMap(geojsonLayer);
   
 }
+
 
 function createMap(earthquakes) {
 
@@ -78,39 +88,31 @@ function createMap(earthquakes) {
 
 
   function getColor(d) {
-      return d < 1 ? 'rgb(255,255,255)' :
-            d < 2  ? 'rgb(255,225,225)' :
-            d < 3  ? 'rgb(255,195,195)' :
-            d < 4  ? 'rgb(255,165,165)' :
-            d < 5  ? 'rgb(255,135,135)' :
-            d < 6  ? 'rgb(255,105,105)' :
-            d < 7  ? 'rgb(255,75,75)' :
-            d < 8  ? 'rgb(255,45,45)' :
-            d < 9  ? 'rgb(255,15,15)' :
-                        'rgb(255,0,0)';
+      return d < 1 ? 'rgb(0,255,0)' :
+            d < 2  ? 'rgb(255,255,0)' :
+            d < 3  ? 'rgb(173,255,47)' :
+            d < 4  ? 'rgb(173,255,47)' :
+            d < 5  ? 'rgb(255,165,0)' :
+            d < 6  ? 'rgb(255,69,0)' :
+            d < 7  ? 'rgb(255,0,0)' :
+                        'rgb(139,0,0)';
   }
 
   // Create a legend to display information about our map
   var legend = L.control({position: 'bottomright'});
 
   legend.onAdd = function (map) {
-  
       var div = L.DomUtil.create('div', 'info legend'),
-      grades = [0, 1, 2, 3, 4, 5, 6, 7, 8],
-      labels = [];
-
+      labels = [1, 2, 3, 4, 5, 6]
       div.innerHTML+='Magnitude<br><hr>'
-  
-      // loop through our density intervals and generate a label with a colored square for each interval
-      for (var i = 0; i < grades.length; i++) {
+        for (var i = 0; i < labels.length; i++) {
           div.innerHTML +=
-              '<i style="background:' + getColor(grades[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
-              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+              '<i style="background:' + getColor(labels[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
+              labels[i] + (labels[i + 1] ? '&ndash;' + labels[i + 1] + '<br>' : '+');
   }
-  
+
   return div;
   };
   
   legend.addTo(myMap);
-
 }
